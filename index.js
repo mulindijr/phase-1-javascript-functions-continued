58b6b4e77e521dfaacff0354e9ad44583c268651
Brian Mulindi <mulindijrn@gmail.com>
2023-09-19 17:16:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(fun = 'roller-skate'){
+    return `This Saturday, I want to ${fun}!`;
+}
+saturdayFun('bathe my dog');
+
+const mondayWork = function(mondayWork = 'go to the office'){
+    return `This Monday, I will ${mondayWork}.`;
+}
+mondayWork('be interviewed');
+
+function wrapAdjective(flair = "*") {
+    return function(name = "special") {
+      return `You are ${flair}${name}${flair}!`;
+    };
+}
+
